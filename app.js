80d5eb1106d84195e13d99666f8a8f32c64cd388
Neenu95 <neenushaji1995@gmail.com>
2020-05-20 20:54:55 -0400

    Added the tetrominoes

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
+    const GRID_WIDTH = 10;
+    //console.log(squares);
 
+    //the tetrominoes
+    //The Tetrominoes
+  const lTetromino = [
+    [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, 2],
+    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 2],
+    [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2],
+    [GRID_WIDTH, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1, GRID_WIDTH * 2 + 2]
+  ]
+
+  const zTetromino = [
+    [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+    [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1],
+    [0, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1],
+    [GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2, GRID_WIDTH * 2 + 1]
+  ]
+
+  const tTetromino = [
+    [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2],
+    [1, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH * 2 + 1],
+    [1, GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1]
+  ]
+
+  const oTetromino = [
+    [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+    [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+    [0, 1, GRID_WIDTH, GRID_WIDTH + 1],
+    [0, 1, GRID_WIDTH, GRID_WIDTH + 1]
+  ]
+
+  const iTetromino = [
+    [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3],
+    [1, GRID_WIDTH + 1, GRID_WIDTH * 2 + 1, GRID_WIDTH * 3 + 1],
+    [GRID_WIDTH, GRID_WIDTH + 1, GRID_WIDTH + 2, GRID_WIDTH + 3]
+  ]
+  
+  const theTetriminoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 })
